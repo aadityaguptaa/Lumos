dedf561176040de5632f84bd7dc11e1efa7af743
Aditya Gupta <52538173+aadityaguptaa@users.noreply.github.com>
2021-12-28 23:33:47 +0530

    initial structure

---
+++ app/src/main/java/com/adityagupta/lumos/LumosApplication.kt
@@ -0,0 +1,8 @@
+package com.adityagupta.lumos
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class LumosApplication:Application() {
+}
---
+++ app/src/main/java/com/adityagupta/lumos/common/Constants.kt
@@ -0,0 +1,8 @@
+package com.adityagupta.lumos.common
+
+object Constants {
+
+    //Base URL for the News API
+    const val BASE_URL = "https://api.coinpaprika.com/"
+
+}
